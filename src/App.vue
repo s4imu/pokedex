ce0faa0827ba3fb2a15a7819d5c7a34879154cbd
s4imu <contatosymon@gmail.com>
2021-03-24 23:47:25 -0400

    Getting data with the API

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="column is-half is-offset-one-quarter">
+      <div v-for="(pokemon, index) in pokemons" :key="index">
+        <Pokemon :name="pokemon.name" :url="pokemon.url" :num="index + 1" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
+import Pokemon from "./components/Pokemon";
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      pokemons: [],
+    };
+  },
+  created: function () {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then((res) => {
+        this.pokemons = res.data.results;
+      });
+  },
   components: {
-    HelloWorld
-  }
-}
+    Pokemon,
+  },
+};
 </script>
-
+  
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="card">
+      <div class="card-image">
+        <figure>
+          <img :src="pokemon.image" :alt="pokemon.name" />
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">{{ num }}. {{ name | upper }}</p>
+            <p class="subtitle is-6">{{ pokemon.type }}</p>
+          </div>
+        </div>
+
+        <div class="content"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  created: function () {
+    axios.get(this.url).then((res) => {
+      this.pokemon.type = res.data.types[0].type.name;
+      this.pokemon.image = res.data.sprites.front_default;
+    });
+  },
+  data() {
+    return {
+      pokemon: {
+        type: "",
+        image: "",
+      },
+    };
+  },
+  props: {
+    num: Number,
+    name: String,
+    url: String,
+  },
+  filters: {
+    upper: function (value) {
+      let newName = value[0].toUpperCase() + value.slice(1);
+      return newName;
+    },
+  },
+};
+</script>
+
+<style>
+#pokemon {
+  margin-top: 2%;
+}
+</style>
